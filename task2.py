e33a4da0e2056fea06127801e17af34181621559
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-09-30 12:23:16 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -15,3 +15,14 @@ outputs:
 Access granted
 Access denied
 """
+
+username = ""
+password = ""
+
+while username != "admin" and password != "12345":
+    username = (input("Enter your username")).strip()
+    password = (input("Enter your password")).strip()
+    if username != "admin" and password != "12345":
+        print("Access denied")
+    elif username == "admin" and password == "12345":
+        print("Access granted")
