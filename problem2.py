71b96a313ff9dc1c2f4c8ede2c353310881588f9
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-09-30 13:03:21 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -15,3 +15,13 @@ example:
 Enter a number: 4
 4 8 12 16 20 24 28 32 36 40 44 48
 """
+
+x =""
+number = (input("Enter an integer")).strip()
+count = 0
+while count <= 12:
+    print(x,end=""+" ")
+    x = int(number)*(count + 1)
+    count = count + 1
+    if count > 12:
+        break
